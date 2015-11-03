09c222099586b067b878522d78a29718e991a104
Shundan Xiao <sherry.xiao@zenpayroll.com>
2015-11-02 19:54:37 -0800

    Adds pentadactylrc file

--- install_dot_files.sh
+++ install_dot_files.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc.before tmux.conf.local vimperatorrc"    # list of files/folders to symlink in homedir
+files="vimrc.before tmux.conf.local vimperatorrc pentadactylrc"    # list of files/folders to symlink in homedir
 
 ##########
 
